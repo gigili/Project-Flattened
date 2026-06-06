f4eb298a52e6eafb9b80c3f545584a0b1e82db12
Igor Ilic <github@igorilic.net>
2026-06-06 11:30:39 +0200

    v2.0.3

--- kubejs/server_scripts/create/assembly/blaze_egg.js
+++ kubejs/server_scripts/create/assembly/blaze_egg.js
@@ -5,9 +5,10 @@ ServerEvents.recipes(event => {
 		Item.of('minecraft:blaze_spawn_egg').withChance(.40),
         Item.of('minecraft:rotten_flesh').withChance(.60)
 	], 'minecraft:egg', [
-		event.recipes.createDeploying(inter, [inter, 'minecraft:gunpowder']),
-		event.recipes.createDeploying(inter, [inter, 'minecraft:blaze_powder']),
+		event.recipes.createFilling(inter, [inter, Fluid.of('minecraft:water', 1000)]),
 		event.recipes.createPressing(inter, inter),
+		event.recipes.createPressing(inter, inter),
+		event.recipes.createFilling(inter, [inter, Fluid.of('minecraft:lava', 1000)]),
 		event.recipes.createFilling(inter, [inter, Fluid.of('minecraft:lava', 1000)])
-	]).transitionalItem(inter).loops(4).id("gac:kjs/create/assembly/blaze_egg");
+	]).transitionalItem(inter).loops(5).id("gac:kjs/create/assembly/blaze_egg");
 });
